fe354df79711107f88282350b94a22bd5293ce25
Kazushi Sakurada <kazushisakurada@icloud.com>
2018-09-05 15:04:10 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Home :msg="message"/>
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Home from './components/Home.vue';
+import Counter from './components/Counter.vue';
+
+export default Vue.component('App', {
+  name: 'app',
+  data() {
+    return {
+      message: 'VueJs Counter',
+    };
+  },
+  components: {
+    Home,
+    Counter,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+<section class="section">
+  <div class="container">
+    <div class="level">
+      <div class="level-item">
+        <button style="padding: 5px 20px" class="button" @click="incrementCounter">Increment</button>
+      </div>
+      <div class="level-item">
+        <h2>{{counter}}</h2>
+      </div>
+      <div class="level-item">
+        <button class="button" style="padding: 5px 20px" @click="decrementCounter">Decrement</button>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.component('Counter', {
+  data() {
+    return {
+      title: 'Counter',
+      counter: 0,
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+    decrementCounter() {
+      this.counter--;
+    },
+  },
+});
+</script>
